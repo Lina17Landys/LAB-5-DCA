8c672cbf61e60ed803ae0e61257107b7bf441e4e
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-06 08:47:29 -0500

    grid

--- src/components/grid.ts
+++ src/components/grid.ts
@@ -1,25 +1,47 @@
 class Grid extends HTMLElement {
+    private pixelCanvas: HTMLTableElement | null;
+    private color: string | null;
+    
     constructor() {
       super();
       this.attachShadow({ mode: "open" });
+      this.pixelCanvas = null;
+      this.color = null; 
     }
   
     connectedCallback() {
-      this.render();
+      const colorPickerId = this.getAttribute("color-picker-id");
+      const colorPicker: HTMLInputElement | null = document.getElementById(colorPickerId) as HTMLInputElement;
+  
+      if (colorPicker) {
+        this.color = colorPicker.value;
+  
+        this.render();
+      } else {
+        console.error("No se encontró el elemento colorPicker");
+      }
     }
   
     render() {
-        this.shadowRoot!.innerHTML = '<link rel="stylesheet" href="../src/components/styles.css">'
+      this.shadowRoot!.innerHTML = '<link rel="stylesheet" href="../src/components/styles.css">';
+  
       const gridSize = 15; 
   
       const table = document.createElement("table");
       table.id = "pixelCanvas";
+      this.pixelCanvas = table; 
+  
+      
       for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("tr");
         for (let j = 0; j < gridSize; j++) {
           const column = document.createElement("td");
           column.id = `column-${i}-${j}`;
           row.appendChild(column);
+  
+          column.addEventListener("click", function () {
+            column.style.backgroundColor = color ? color.value : "";
+          });
         }
         table.appendChild(row);
       }
@@ -28,5 +50,5 @@ class Grid extends HTMLElement {
     }
   }
   
-  customElements.define("pixel-grid", Grid);  
-export default Grid;  
+  customElements.define("pixel-grid", Grid);
+  
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ let color: HTMLInputElement | null = document.getElementById("colorPicker") as H
 
 const gridContainer: HTMLElement | null = document.getElementById("gridContainer");
 
+const pixelGrid: any = document.querySelector("pixel-grid");
+pixelGrid.color = color ? color.value : "";
 gridContainer?.addEventListener("click", function (e) {
   if (e.target instanceof HTMLTableCellElement) {
     e.target.style.backgroundColor = color ? color.value : "";
   }
 });
+
+pixelGrid.createGrid();
