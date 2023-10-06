b367f52dbcb81985d20c31f738f879d63a976a2d
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-06 07:33:02 -0500

    grid

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,42 @@
-console.log("hola");
-import * as components from "./components/exports"
-class App extends HTMLElement {
-    constructor() {
-        super()
-        this.attachShadow({ mode: "open" });
-    }
-    connectedCallback() {
-        this.render();
-      }
+// Select color input
+let color: HTMLInputElement | null = document.getElementById("colorPicker") as HTMLInputElement;
 
-      render() {
-        this.shadowRoot!.innerHTML = '<grid-container></grid-container> '
-      }
+const pixelCanvas: HTMLElement | null = document.getElementById("pixelCanvas");
+
+function createGrid() {
+  // Clear the grid if it already exists
+  clearGrid();
+
+  const gridSize = 15; // Define the grid size as 15x15
+
+  // Create the 15x15 grid
+  for (let i = 0; i < gridSize; i++) {
+    const row: HTMLTableRowElement = document.createElement("tr");
+    for (let j = 0; j < gridSize; j++) {
+      const column: HTMLTableCellElement = document.createElement("td");
+      column.id = `column-${i}-${j}`;
+      row.appendChild(column);
+    }
+    if (pixelCanvas) {
+      pixelCanvas.appendChild(row);
+    }
+  }
 }
 
-customElements.define("app-container", App);
+function clearGrid() {
+  if (pixelCanvas) {
+    pixelCanvas.innerHTML = ""; // Clear the entire grid
+  }
+}
+
+if (pixelCanvas) {
+  // Create the initial 15x15 grid
+  createGrid();
+
+  // Add click event listener to color the cells
+  pixelCanvas.addEventListener("click", function (e) {
+    if (e.target instanceof HTMLTableCellElement) {
+      e.target.style.backgroundColor = color ? color.value : "";
+    }
+  });
+}
